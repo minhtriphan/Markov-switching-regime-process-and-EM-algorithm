7118d0dcd8f557ffa6b933b142796f6273f592b3
minhtriphan <39737967+minhtriphan@users.noreply.github.com>
2018-12-06 00:01:59 +0100

    Add files via upload

--- MSAR_and_EM.m
+++ MSAR_and_EM.m
@@ -1,3 +1,4 @@
+%%
 % Simulation
 % Transition probabilities
 p11 = 0.9;
@@ -37,10 +38,11 @@ end
 y = y(100:end);
 figure;
 plot(y);
-
+%%
+y = log(Value(2:end)./Value(1:end-1));
 
 % Estimation
-S = 100;    % number of iterations
+S = 50;    % number of iterations
 para = zeros(8,S);
 f_prob = zeros(2,length(y)-1);
 u_prob = zeros(2,length(y));
@@ -74,9 +76,10 @@ ylim([0,1]);
 title('smoothed probabilites');
 subplot(2,1,2);
 plot(f_prob(1,:));
+xlim([0,length(s_prob(1,:))]);
 ylim([0,1]);
 title('filtering probabilities');
-
+%%
 % Final parameters
 final_para = para(:,end);
 fprintf('True intercepts of 2 regimes: %2.1f (1st regime), and %2.1f (2nd regime)\n', a);
@@ -87,3 +90,45 @@ fprintf('True standard deviations of 2 regimes: %2.1f (1st regime), and %2.1f (2
 fprintf('Estimated standard deviations of 2 regimes: %2.3f (1st regime), and %2.3f (2nd regime)\n', final_para(5:6));
 fprintf('Transition probabilities: %2.1f (stay in 1st regime), and %2.1f (stay in 2nd regime)\n', [p11; p22]);
 fprintf(['Estimated transition probabilities: %2.3f (stay in 1st regime), and %2.3f (stay in 2nd regime)\n'], final_para(7:8));
+
+figure;
+subplot(4,2,1);
+plot(1:S, para(1,:), 'Linewidth', 1.5); hold on
+plot(1:S, repmat(a(1),[1,S]), 'r-', 'Linewidth', 1.5);
+title('Intercept, regime 1');
+
+subplot(4,2,2);
+plot(1:S, para(2,:), 'Linewidth', 1.5); hold on
+plot(1:S, repmat(a(2),[1,S]), 'r-', 'Linewidth', 1.5);
+title('Intercept, regime 2');
+
+subplot(4,2,3);
+plot(1:S, para(3,:), 'Linewidth', 1.5); hold on
+plot(1:S, repmat(b(1),[1,S]), 'r-', 'Linewidth', 1.5);
+title('Slope, regime 1');
+
+subplot(4,2,4);
+plot(1:S, para(4,:), 'Linewidth', 1.5); hold on
+plot(1:S, repmat(b(2),[1,S]), 'r-', 'Linewidth', 1.5);
+title('Slope, regime 2');
+
+subplot(4,2,5);
+plot(1:S, para(5,:), 'Linewidth', 1.5); hold on
+plot(1:S, repmat(sig(1),[1,S]), 'r-', 'Linewidth', 1.5);
+title('Standard Deviation, regime 1');
+
+subplot(4,2,6);
+plot(1:S, para(6,:), 'Linewidth', 1.5); hold on
+plot(1:S, repmat(sig(2),[1,S]), 'r-', 'Linewidth', 1.5);
+title('Standard Deviation, regime 2');
+
+subplot(4,2,7);
+plot(1:S, para(7,:), 'Linewidth', 1.5); hold on
+plot(1:S, repmat(p11,[1,S]), 'r-', 'Linewidth', 1.5);
+title('Transition probability from state 1 to state 1');
+
+subplot(4,2,8);
+plot(1:S, para(7,:), 'Linewidth', 1.5); hold on
+plot(1:S, repmat(p22,[1,S]), 'r-', 'Linewidth', 1.5);
+title('Transition probability from state 2 to state 2');
+legend('Estimated value', 'True value');
